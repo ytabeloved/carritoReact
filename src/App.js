a1803af26971cb12f2c25efa3f92bbce16ac9d1e
Ytabeloved <karlisvaldi@gmail.com>
2021-08-23 20:01:14 -0400

    inicio de repo carrito coder

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('Hello world')
+
+
   return (
     <div className="App">
       <header className="App-header">
