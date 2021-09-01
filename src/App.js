68ff1456430faa5b60a6103053e1e2bf303fc5eb
Ytabeloved <karlisvaldi@gmail.com>
2021-09-01 18:48:24 -0400

    navbar desafio 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
+import NavBar from './components/navbar';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 
 function App() {
   console.log('Hello world')
@@ -7,19 +11,10 @@ function App() {
 
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+
       </header>
     </div>
   );
