bcbb3594b75d26167336dd4ebdec5ed04b248a7f
Ytabeloved <karlisvaldi@gmail.com>
2021-09-27 21:38:00 -0300

    eliminando comentados

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -54,30 +54,7 @@ const Login = () => {
       </Form>
       </div>
 
-      /*
-        <div>
-          <div>SignIn</div>
-          <form onSubmit={handleLogin}>
-            <div>Username
-              <input
-                type='text'
-                value={username}
-                onChange={({ target }) => setUsername(target.value)}
-              />
-            </div>
-            <div>Password
-              <input
-                type='password'
-                value={password}
-                onChange={({ target }) => setPassword(target.value)}
-              />
-            </div>
-            <button type='submit'>login</button>
-          </form>
-        </div>*/
-
-
-      )
+           )
 }
 
 export default Login
